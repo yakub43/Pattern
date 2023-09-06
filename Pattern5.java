c2b98964ecd7ebeea9671fe422679383c8c807dd
yakub43 <yakubakku43@gmail.com>
2023-09-06 10:57:49 +0530

    Code for pattern5 done

--- Pattern5.java
+++ Pattern5.java
@@ -1,11 +1,12 @@
 public class Pattern2 {
     public static void main(String[] args) {
-        pattern4(4);
+        pattern5(4);
     }
-    static void pattern4(int n){
+    static void pattern5(int n){
         for (int row = 1; row <= n; row++){
             // for every row, run the col
-            for (int col = 1; col <= row; col++){
+            int totalColsInRow = row > n ? 2*n - row : row;
+            for (int col = 1; col < totalColsInRow; col++){
                 System.out.print(col+" ");
             }
             // when one row is printed, we need to add a newline
